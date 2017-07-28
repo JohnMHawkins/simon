7a5a8b65fde4a0bd1d0f178ccc2da4293621a859
ReRamp <aidan@detonatorgames.com>
2017-07-28 00:40:01 -0700

    Arduino Added Commands and new Parsing System

--- arduino/Simon_Porto_Mark3/Simon_Porto_Mark3.ino
+++ arduino/Simon_Porto_Mark3/Simon_Porto_Mark3.ino
@@ -16,7 +16,8 @@ enum LedState {
   WEIGHT,
   PATTERN,
   RAINBOW,
-  COUNTDOWN
+  COUNTDOWN,
+  FLASH
 };
 
 
@@ -42,9 +43,23 @@ HX711 loadCell;
 CRGB leds[NUM_LEDS];
 
 // vars for serial communication
+// vars for serial communication
 String inputString = "";
+String simonCommand = "";
 boolean stringComplete = false;
 String deviceName = "unknown";
+String deviceInput = "unknown";
+
+//Arduino Identification Pins (set to ground)
+int PIN_CENTER = 51;
+
+int PIN_BLUEGREEN = 52;
+int PIN_REDYELLOW = 53;
+
+int PIN_BLUE = 50;
+int PIN_RED = 49;
+int PIN_GREEN = 48;
+int PIN_YELLOW = 47;
 
 //settup threads
 //ThreadController tController = ThreadController();
@@ -77,6 +92,12 @@ int tickTime;
 int totalTime;
 int trailLength = 19;
 int countdownIndex = 0;
+
+//flashLed vars
+int flashCount;
+int flashTime;
+int flashIndex = 0;
+CRGB flashColor;
 
 //init weightColor vals
 int weightModifier = 1;
@@ -113,6 +134,10 @@ void gameOver()
 void setup() {
   Timer3.initialize(1000000);
 
+  randomSeed(analogRead(0));
+  //setup arduino identification pins
+  setupIDPin();
+
   //init serial
   Serial.begin (115200);
   Serial.setTimeout (50);
@@ -138,6 +163,24 @@ void setup() {
   delay(1000);
   FastLED.setBrightness(BRIGHTNESS);
   FastLED.show();
+}
+
+void setupIDPin() {
+  pinMode(PIN_CENTER, INPUT);
+  pinMode(PIN_BLUEGREEN, INPUT);
+  pinMode(PIN_REDYELLOW, INPUT);
+  pinMode(PIN_BLUE, INPUT);
+  pinMode(PIN_GREEN, INPUT);
+  pinMode(PIN_RED, INPUT);
+  pinMode(PIN_YELLOW, INPUT);
+
+  digitalWrite(PIN_CENTER, HIGH);
+  digitalWrite(PIN_BLUEGREEN, HIGH);
+  digitalWrite(PIN_REDYELLOW, HIGH);
+  digitalWrite(PIN_BLUE, HIGH);
+  digitalWrite(PIN_GREEN, HIGH);
+  digitalWrite(PIN_RED, HIGH);
+  digitalWrite(PIN_YELLOW, HIGH);
 }
 
 void loop() {
@@ -169,6 +212,9 @@ void loop() {
     case COUNTDOWN:
       countdownLeds();
       break;
+
+    case FLASH:
+      break;
   }
 
   noInterrupts();
@@ -186,66 +232,95 @@ void loop() {
 //
 void simonOutput (String outputString) {
   Serial.print(deviceName);
+  Serial.print("[" + deviceInput + "]");
   Serial.print(":");
   Serial.println(outputString);
 }
 
-//
-// function to compare our inputString to possible commands
-// add more commands and calls to funcitons here
-//
 void parseCommand () {
 
   //our giant if statement to compare all commands
 
+  //assign device nmae
   if (inputString.substring(0, 4) == "NAME" ) {
+    //assign device nmae
     simonAssignDeviceName();
 
-  } else if (inputString.substring(0, 10) == "FLASHCOLOR" ) {
+  } else {
+    //get deviceInputName and process command
+    deviceInput = getValue(inputString, ':', 0);
+    simonCommand = getValue(inputString, ':', 1);
+    //Serial.println(deviceInput + "  " + simonCommand);
+
+    processSimonCommand();
+  }
+
+  // clear inputString, deviceInput & commandString
+  inputString = "";
+  deviceInput = "";
+  simonCommand = "";
+  stringComplete = false;
+
+}
+
+void processSimonCommand() {
+
+  //  if (inputString.substring(0,10) == "FLASHCOLOR" ) {
+  if (simonCommand == "FLASHCOLOR") {
     //simonOutput(inputString);
-    String colorRGB = getValue(inputString, ':', 1);
+    String colorRGB = getValue(inputString, ':', 2);
 
     String colorRed = getValue(colorRGB, '-', 0);
     String colorGreen = getValue(colorRGB, '-', 1);
     String colorBlue = getValue(colorRGB, '-', 2);
 
-    String colorTime = getValue(inputString, ':', 2);
+    String flashTime = getValue(inputString, ':', 3);
+
+    String flashAmmount = getValue(inputString, ':', 4);
+    simonOutput(deviceInput + ":" + colorRed + ":" + colorGreen + ":" + colorBlue + " FLASHTIME:" + flashTime + " FLASHAMMOUNT:" + flashAmmount);
 
     CRGB _color;
     _color.r = colorRed.toInt();
     _color.g = colorGreen.toInt();
     _color.b = colorBlue.toInt();
 
-    setColorFlash(_color, colorTime.toInt());
-    simonOutput("COLOR:" + colorRed + "::" + colorGreen + "::" + colorBlue + " TIME:" + colorTime);
+    flashColors(_color, flashTime.toInt(), flashAmmount.toInt());
 
-  } else if (inputString == "GS_ATTRACT") {
-    simonOutput (inputString);
+  } else if (simonCommand == "GS_ATTRACT") {
+    simonOutput (simonCommand);
     attract();
     //Call your attract function here
 
-  } else if (inputString == "GS_COMPUTER") {
-    simonOutput (inputString);
+  } else if (simonCommand == "GS_COMPUTER") {
+    simonOutput (simonCommand);
     setSolidColor(CRGB::Orange);
     //Call compiter function here
 
-  } else if (inputString == "GS_TIMER") {
-    simonOutput (inputString);
-    //Call your timer function here
-    countdown(CRGB::White,  1000G);
+  } else if (simonCommand == "GS_TIMER") {
+    simonOutput (simonCommand);
+    String colorRGB = getValue(inputString, ':', 2);
 
-  } else if (inputString == "READ_BUTTONS") {
+    String colorRed = getValue(colorRGB, '-', 0);
+    String colorGreen = getValue(colorRGB, '-', 1);
+    String colorBlue = getValue(colorRGB, '-', 2);
+
+    String colorTime = getValue(inputString, ':', 3);
+    simonOutput(deviceInput + ":" + colorRed + ":" + colorGreen + ":" + colorBlue + " TIME:" + colorTime);
+
+    CRGB _color;
+    _color.r = colorRed.toInt();
+    _color.g = colorGreen.toInt();
+    _color.b = colorBlue.toInt();
+
+    countdown(_color,  colorTime.toInt());
+
+  } else if (simonCommand == "READ_BUTTONS") {
     //create test button data, replace with real button function
-    //Serial.println(loadCell.read());
     simonOutput ("BTTN:" + String(getWeight()));
 
   } else {
-    simonOutput ("NOCMD:" + inputString);
+    simonOutput ("NOCMD:" + simonCommand);
   }
-
-  // clear inputString
-  inputString = "";
-  stringComplete = false;
 
 }
 
@@ -298,9 +373,35 @@ String getValue(String data, char separator, int index)
 
 void simonAssignDeviceName() {
 
-  deviceName = getValue(inputString, ':', 1);
+  //deviceName = getValue(inputString, ':', 1);
 
-  Serial.print("DEVICE ASSIGNED : ");
+  int valCENTER = digitalRead(PIN_CENTER);
+  int valBLUEGREEN = digitalRead(PIN_BLUEGREEN);
+  int valREDYELLOW = digitalRead(PIN_REDYELLOW);
+  int valBLUE = digitalRead(PIN_BLUE);
+  int valGREEN = digitalRead(PIN_GREEN);
+  int valRED = digitalRead(PIN_RED);
+  int valYELLOW = digitalRead(PIN_YELLOW);
+
+  if (valCENTER == LOW) {
+    deviceName = "CENTER";
+  } else if (valBLUEGREEN == LOW) {
+    deviceName = "BLUEGREEN";
+  } else if (valREDYELLOW == LOW) {
+    deviceName = "REDYELLOW";
+  } else if (valBLUE == LOW) {
+    deviceName = "BLUE";
+  } else if (valGREEN == LOW) {
+    deviceName = "GREEN";
+  } else if (valRED == LOW) {
+    deviceName = "RED";
+  } else if (valYELLOW == LOW) {
+    deviceName = "YELLOW";
+  } else {
+    deviceName = "ERROR:UNKNOWN";
+  }
+
+  Serial.print("NAME:");
   Serial.println(deviceName);
 
 }
@@ -395,6 +496,61 @@ void countdownLeds()
 
 }
 
+void flashColors(CRGB _color, int _time, int _ammount)
+{
+  flashCount = _ammount;
+  flashTime = _time;
+  flashIndex = 0;
+  flashColor = _color;
+  ledState = FLASH;
+
+  unsigned long _interval = 1000l * (flashTime / 2);
+  Timer3.setPeriod(_interval);
+  Timer3.restart();
+  Timer3.attachInterrupt(flashColorsCallback);
+}
+
+void flashColorsCallback()
+{
+  if (flashIndex % 2 == 1)
+  {
+    fill_solid(leds, NUM_LEDS, CRGB::Black);
+  }
+  else
+  {
+    fill_solid(leds, NUM_LEDS, flashColor);
+  }
+  flashIndex ++;
+  if (flashIndex > flashCount * 2)
+  {
+    fill_solid(leds, NUM_LEDS, CRGB::Black);
+    Timer3.detachInterrupt();
+  }
+}
+
+////lightStreak vars
+//int steakAmmount;
+//int minStreakTime;
+//int maxSteakTime;
+//int streakFade;
+//
+//CRGB streaks[99];
+//int streakPos[99]
+//int streakSpeeds[99]
+//
+//int streakColorAmmount = 3;
+//CRGB streakColors = {CRGB::Red, CRGB::Green, CRGB::Blue);
+//
+//void lightStreak (int _ammount, int minTime, int maxTime, int fade)
+//{
+//  
+//}
+//
+//void lightStrakCallback
+//{
+//
+//}
+
 void countdown(CRGB _color, int _time)
 {
   solidColor = _color;
@@ -413,7 +569,7 @@ void countdownCallback () {
   fadeToBlackBy( leds, NUM_LEDS, 64);
 
   int cIndex = countdownIndex;
-  if(cIndex > tickNumber)
+  if (cIndex > tickNumber)
   {
     cIndex = tickNumber;
   }
