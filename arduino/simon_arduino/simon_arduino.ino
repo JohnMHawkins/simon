e00628b60e7ab95feaa9333258eb62c8d5d2eb3d
ReRamp <aidan@detonatorgames.com>
2020-07-12 10:29:15 -0800

    latest simon code

--- arduino/simon_arduino/simon_arduino.ino
+++ arduino/simon_arduino/simon_arduino.ino
@@ -58,177 +58,276 @@ void updateCenterButton()
 
 // new button
 
-const byte dataPins[4][4] = {{4, 5, 6, 6}, {14, 22, 24, 25}, {A0, A1, A2, A3}, {A4, A9, A10, A11}} ; //{{4, 5, 6, 7}, {14, 27, 28, 29}, {A0, A1, A2, A3}, {A8, A9, A10, A11}};
+const byte dataPins[4][4] = {{4, 5, 6, 7}, {14, 22, 24, 25}, {A0, A1, A2, A3}, {A4, A9, A10, A11}} ; //{{4, 5, 6, 7}, {14, 27, 28, 29}, {A0, A1, A2, A3}, {A8, A9, A10, A11}};
 const byte clockPins[4][4] = {{3, 3, 3, 3}, {2, 2, 2, 2}, {3, 3, 3, 3}, {2, 2, 2, 2}};
 //HX711 loadCell[4][4];
-#define mWeight 160000
+
+
+
+
+#define mWeight 600
+
+
+
+
+
 //long maxWeight[4] = {mWeight, mWeight, mWeight, mWeight};
 
 HX711_ADC lc_0_0(dataPins[0][0], clockPins[0][0]);
 HX711_ADC lc_0_1(dataPins[0][1], clockPins[0][1]);
-HX711_ADC lc_0_2(dataPins[0][2], clockPins[0][2]);
-HX711_ADC lc_0_3(dataPins[0][3], clockPins[0][3]);
+//HX711_ADC lc_0_2(dataPins[0][2], clockPins[0][2]);
+//HX711_ADC lc_0_3(dataPins[0][3], clockPins[0][3]);
 
 HX711_ADC lc_1_0(dataPins[1][0], clockPins[1][0]);
 HX711_ADC lc_1_1(dataPins[1][1], clockPins[1][1]);
-HX711_ADC lc_1_2(dataPins[1][2], clockPins[1][2]);
-HX711_ADC lc_1_3(dataPins[1][3], clockPins[1][3]);
+//HX711_ADC lc_1_2(dataPins[1][2], clockPins[1][2]);
+//HX711_ADC lc_1_3(dataPins[1][3], clockPins[1][3]);
 
 HX711_ADC lc_2_0(dataPins[2][0], clockPins[2][0]);
 HX711_ADC lc_2_1(dataPins[2][1], clockPins[2][1]);
-HX711_ADC lc_2_2(dataPins[2][2], clockPins[2][2]);
-HX711_ADC lc_2_3(dataPins[2][3], clockPins[2][3]);
+//HX711_ADC lc_2_2(dataPins[2][2], clockPins[2][2]);
+//HX711_ADC lc_2_3(dataPins[2][3], clockPins[2][3]);
 
 HX711_ADC lc_3_0(dataPins[3][0], clockPins[3][0]);
 HX711_ADC lc_3_1(dataPins[3][1], clockPins[3][1]);
-HX711_ADC lc_3_2(dataPins[3][2], clockPins[3][2]);
-HX711_ADC lc_3_3(dataPins[3][3], clockPins[3][3]);
+//HX711_ADC lc_3_2(dataPins[3][2], clockPins[3][2]);
+//HX711_ADC lc_3_3(dataPins[3][3], clockPins[3][3]);
 
 
 void setupCells() {
 
   lc_0_0.begin();
   lc_0_1.begin();
-  lc_0_2.begin();
-  lc_0_3.begin();
+//  lc_0_2.begin();
+//  lc_0_3.begin();
 
   lc_1_0.begin();
   lc_1_1.begin();
-  lc_1_2.begin();
-  lc_1_3.begin();
+//  lc_1_2.begin();
+//  lc_1_3.begin();
 
   lc_2_0.begin();
   lc_2_1.begin();
-  lc_2_2.begin();
-  lc_2_3.begin();
+//  lc_2_2.begin();
+//  lc_2_3.begin();
 
   lc_3_0.begin();
   lc_3_1.begin();
-  lc_3_2.begin();
-  lc_3_3.begin();
+//  lc_3_2.begin();
+//  lc_3_3.begin();
 
-  lc_0_0.start(200);
-  lc_0_1.start(200);
-  lc_0_2.start(200);
-  lc_0_3.start(200);
+  lc_0_0.start(2000);
+  lc_0_1.start(2000);
+//  lc_0_2.start(2000);
+//  lc_0_3.start(2000);
 
-  lc_1_0.start(200);
-  lc_1_1.start(200);
-  lc_1_2.start(200);
-  lc_1_3.start(200);
+  lc_1_0.start(2000);
+  lc_1_1.start(2000);
+//  lc_1_2.start(2000);
+//  lc_1_3.start(2000);
 
-  lc_2_0.start(200);
-  lc_2_1.start(200);
-  lc_2_2.start(200);
-  lc_2_3.start(200);
+  lc_2_0.start(2000);
+  lc_2_1.start(2000);
+//  lc_2_2.start(2000);
+//  lc_2_3.start(2000);
 
-  lc_3_0.start(200);
-  lc_3_1.start(200);
-  lc_3_2.start(200);
-  lc_3_3.start(200);
+  lc_3_0.start(2000);
+  lc_3_1.start(2000);
+//  lc_3_2.start(2000);
+//  lc_3_3.start(2000);
+
+  //  lc_0_0.startMultiple(2000);
+  //  lc_0_1.startMultiple(2000);
+  //  lc_0_2.startMultiple(2000);
+  //  lc_0_3.startMultiple(2000);
+  //
+  //  lc_1_0.startMultiple(2000);
+  //  lc_1_1.startMultiple(2000);
+  //  lc_1_2.startMultiple(2000);
+  //  lc_1_3.startMultiple(2000);
+  //
+  //  lc_2_0.startMultiple(2000);
+  //  lc_2_1.startMultiple(2000);
+  //  lc_2_2.startMultiple(2000);
+  //  lc_2_3.startMultiple(2000);
+  //
+  //  lc_3_0.startMultiple(2000);
+  //  lc_3_1.startMultiple(2000);
+  //  lc_3_2.startMultiple(2000);
+  //  lc_3_3.startMultiple(2000);
+
+  const int cf = 696;
+
+  lc_0_0.setCalFactor(cf);
+  lc_0_1.setCalFactor(cf);
+//  lc_0_2.setCalFactor(cf);
+//  lc_0_3.setCalFactor(cf);
+
+  lc_1_0.setCalFactor(cf);
+  lc_1_1.setCalFactor(cf);
+//  lc_1_2.setCalFactor(cf);
+//  lc_1_3.setCalFactor(cf);
+
+  lc_2_0.setCalFactor(cf);
+  lc_2_1.setCalFactor(cf);
+//  lc_2_2.setCalFactor(cf);
+//  lc_2_3.setCalFactor(cf);
+
+  lc_3_0.setCalFactor(cf);
+  lc_3_1.setCalFactor(cf);
+//  lc_3_2.setCalFactor(cf);
+//  lc_3_3.setCalFactor(cf);
 
 
   /*
-  for (int i = 0; i < 4; i++)
-  {
+    for (int i = 0; i < 4; i++)
+    {
     for (int j = 0; j < 4; j++)
     {
       //Serial.println("test:");
       loadCell[i][j].begin(dataPins[i][j], clockPins[i][j]);
       //loadCell[i][j].tare();
     }
-  }
+    }
   */
 }
 
 void zeroCells() {
 
-  lc_0_0.tareNoDelay();
-  lc_0_1.tareNoDelay();
-  lc_0_2.tareNoDelay();
-  lc_0_3.tareNoDelay();
+  lc_0_0.tare();
+  lc_0_1.tare();
+//  lc_0_2.tare();
+//  lc_0_3.tare();
 
-  lc_1_0.tareNoDelay();
-  lc_1_1.tareNoDelay();
-  lc_1_2.tareNoDelay();
-  lc_1_3.tareNoDelay();
+  lc_1_0.tare();
+  lc_1_1.tare();
+//  lc_1_2.tare();
+//  lc_1_3.tare();
 
-  lc_2_0.tareNoDelay();
-  lc_2_1.tareNoDelay();
-  lc_2_2.tareNoDelay();
-  lc_2_3.tareNoDelay();
+  lc_2_0.tare();
+  lc_2_1.tare();
+//  lc_2_2.tare();
+//  lc_2_3.tare();
 
-  lc_3_0.tareNoDelay();
-  lc_3_1.tareNoDelay();
-  lc_3_2.tareNoDelay();
-  lc_3_3.tareNoDelay();
+  lc_3_0.tare();
+  lc_3_1.tare();
+//  lc_3_2.tare();
+//  lc_3_3.tare();
 
 
-  /*  
-  for (int i = 0; i < 4; i++)
-  {
+
+  //  lc_0_0.tareNoDelay();
+  //  lc_0_1.tareNoDelay();
+  //  lc_0_2.tareNoDelay();
+  //  lc_0_3.tareNoDelay();
+  //
+  //  lc_1_0.tareNoDelay();
+  //  lc_1_1.tareNoDelay();
+  //  lc_1_2.tareNoDelay();
+  //  lc_1_3.tareNoDelay();
+  //
+  //  lc_2_0.tareNoDelay();
+  //  lc_2_1.tareNoDelay();
+  //  lc_2_2.tareNoDelay();
+  //  lc_2_3.tareNoDelay();
+  //
+  //  lc_3_0.tareNoDelay();
+  //  lc_3_1.tareNoDelay();
+  //  lc_3_2.tareNoDelay();
+  //  lc_3_3.tareNoDelay();
+
+
+  /*
+    for (int i = 0; i < 4; i++)
+    {
     for (int j = 0; j < 4; j++)
     {
       loadCell[i][j].tare();
     }
     //Serial.println(maxWeight[i]);
     maxWeight[i] = mWeight;
-  }
+    }
   */
-  
+
 }
 
 byte getCellWeight(int Button)
 {
+
   float weight = 0;
 
   switch (Button) {
     case BTN_RED:
+      weight = weight + lc_0_0.update();
+      weight = weight + lc_0_1.update();
+//      weight = weight + lc_0_2.update();
+//      weight = weight + lc_0_3.update();
+      //
       weight = weight + lc_0_0.getData();
       weight = weight + lc_0_1.getData();
-      weight = weight + lc_0_2.getData();
-      weight = weight + lc_0_3.getData();
+//      weight = weight + lc_0_2.getData();
+//      weight = weight + lc_0_3.getData();
       break;
 
     case BTN_GREEN:
+      weight = weight + lc_1_0.update();
+      weight = weight + lc_1_1.update();
+//      weight = weight + lc_1_2.update();
+//      weight = weight + lc_1_3.update();
+      //
       weight = weight + lc_1_0.getData();
       weight = weight + lc_1_1.getData();
-      weight = weight + lc_1_2.getData();
-      weight = weight + lc_1_3.getData();
+//      weight = weight + lc_1_2.getData();
+//      weight = weight + lc_1_3.getData();
       break;
 
     case BTN_BLUE:
+      weight = weight + lc_2_0.update();
+      weight = weight + lc_2_1.update();
+//      weight = weight + lc_2_2.update();
+//      weight = weight + lc_2_3.update();
+      //
       weight = weight + lc_2_0.getData();
       weight = weight + lc_2_1.getData();
-      weight = weight + lc_2_2.getData();
-      weight = weight + lc_2_3.getData();
+//      weight = weight + lc_2_2.getData();
+//      weight = weight + lc_2_3.getData();
       break;
 
     case BTN_YELLOW:
+      weight = weight + lc_3_0.update();
+      weight = weight + lc_3_1.update();
+//      weight = weight + lc_3_2.update();
+//      weight = weight + lc_3_3.update();
+      //
       weight = weight + lc_3_0.getData();
       weight = weight + lc_3_1.getData();
-      weight = weight + lc_3_2.getData();
-      weight = weight + lc_3_3.getData();
+//      weight = weight + lc_3_2.getData();
+//      weight = weight + lc_3_3.getData();
       break;
-    
+
+    default:
+      weight = -1;
+      break;
+
   }
 
-  weight = weight / 4.0;
-  byte normalWeight = (byte)(255 * weight / (4.0*mWeight));
+//  weight = weight / 4.0;
+  if(weight > mWeight) weight = mWeight;
+  byte normalWeight = (byte)(255 * weight / (4.0 * mWeight));
 
-  //Serial.println("Cell [" + (String)Button + "]: " + (String)weight);
+  Serial.println("Button: [" + (String)Button + "]: " + (String)weight);
 
   return normalWeight;
 
   /*
-  
-  int button = Button - 1;
 
-  long weight = 0;
+    int button = Button - 1;
+
+    long weight = 0;
 
 
-  for (int i = 0; i < 4; i++)
-  {
+    for (int i = 0; i < 4; i++)
+    {
     //Serial.println("getting load cell data cell: " + (String)i);
     long w = loadCell[button][i].get_value(1);
     if(button == BTN_YELLOW )
@@ -241,20 +340,20 @@ byte getCellWeight(int Button)
     //{
     //Serial.println("Cell [" + (String)button + "][" + (String)i + "]: " + (String)w);
     //}
-  }
+    }
 
 
-  
-  weight /= 4;
 
-  if (weight > mWeight) weight = mWeight;
+    weight /= 4;
 
-  byte normalWeight = (byte)((double)weight / (mWeight) * 255);
+    if (weight > mWeight) weight = mWeight;
 
-  //if(button == 2)
-  //Serial.println(normalWeight);
-  
-  return normalWeight;
+    byte normalWeight = (byte)((double)weight / (mWeight) * 255);
+
+    //if(button == 2)
+    //Serial.println(normalWeight);
+
+    return normalWeight;
   */
 }
 
@@ -278,14 +377,14 @@ void setup() {
   //init buttons
 
   Serial.begin(9600);
-  Serial.println("Initing...");
+  //Serial.println("Initing...");
 
   setupCells();
-
   setupCenterButton();
-  zeroCells();
 
   //Serial.println("Buttons Inited!");
+
+  zeroCells();
 
   //Serial.println("Finished!");
 
@@ -294,6 +393,7 @@ void setup() {
 }
 
 void loop() {
+  Wire.begin(SLAVE_ADDRESS);
   // put your main code here, to run repeatedly:
   updateCenterButton();
 
@@ -304,13 +404,13 @@ void loop() {
   weights[3] = getWeight(3);
   weights[4] = getWeight(4);
 
-  /*
-  Serial.println("\n\nGetting buttons:");
+
+  //Serial.println("\n\nGetting buttons:");
   for (int i = 0; i < 5; i++)
   {
-    Serial.println(weights[i]);
+    //Serial.println(weights[i]);
   }
-  */
+
   yield();
 }
 
@@ -366,9 +466,9 @@ byte getWeight(int btn) {
     case BTN_RED:
       //return 255;
       d = getCellWeight(BTN_RED);
-      if(d > 0)
+      if (d > 0)
       {
-      return d;
+        return d;
       }
       else
       {
@@ -378,9 +478,9 @@ byte getWeight(int btn) {
     case BTN_GREEN:
       //return 0;
       d = getCellWeight(BTN_GREEN);
-      if(d > 0)
+      if (d > 0)
       {
-      return d;
+        return d;
       }
       else
       {
@@ -390,9 +490,9 @@ byte getWeight(int btn) {
     case BTN_BLUE:
       //return 0;
       d = getCellWeight(BTN_BLUE);
-      if(d > 0)
+      if (d > 0)
       {
-      return d;
+        return d;
       }
       else
       {
@@ -402,9 +502,9 @@ byte getWeight(int btn) {
     case BTN_YELLOW:
       // return 0;
       d = getCellWeight(BTN_YELLOW);
-      if(d > 0)
+      if (d > 0)
       {
-      return d;
+        return d;
       }
       else
       {
